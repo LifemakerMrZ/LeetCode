b65041d30bfd9631f584a0d9dbbed5b1cbfeeba8
LifeMakerMac <1784658869@qq.com>
2020-06-04 17:08:43 +0800

    6/4

--- src/Add_Two_Numbers.java
+++ src/Add_Two_Numbers.java
@@ -3,17 +3,66 @@
  * User: zs
  * Date: 2020/6/4
  * Time: 16:30
- * 
- * Description:You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
- *
+ * <p>
+ * Description:#2
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * <p>
  * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
  * Output: 7 -> 0 -> 8
  * Explanation: 342 + 465 = 807.
  */
 
-public class Add_Two_Numbers {
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
 }
+
+public class Add_Two_Numbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1=new ListNode(2);
+        l1.next=new ListNode(4);
+        l1.next.next=new ListNode(3);
+        ListNode l2=new ListNode(5);
+        l2.next=new ListNode(6);
+        l2.next.next=new ListNode(4);
+        Add_Two_Numbers ad=new Add_Two_Numbers();
+        ListNode l3=ad.addTwoNumbers(l1,l2);
+        System.out.println(l3.val);
+    }
+}
+
+
---
+++ src/FourSum.java
@@ -0,0 +1,31 @@
+/**
+ * IDE : IntelliJ IDEA
+ * User: zs
+ * Date: 2020/6/4
+ * Time: 17:07
+ * <p>
+ * Description:#18
+ * Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+ * <p>
+ * Note:
+ * <p>
+ * The solution set must not contain duplicate quadruplets.
+ * <p>
+ * Example:
+ * <p>
+ * Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+ * <p>
+ * A solution set is:
+ * [
+ * [-1,  0, 0, 1],
+ * [-2, -1, 1, 2],
+ * [-2,  0, 0, 2]
+ * ]
+ */
+
+public class FourSum {
+    
+    public static void main(String[] args) {
+        
+    }
+}
